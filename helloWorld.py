9a47ecbc3d08de3ba71e2bb36e298c9638a472da
Seabert <74805166+SeabertYuan@users.noreply.github.com>
2022-01-05 16:52:28 -0700

    Add files via upload

---
+++ helloWorld.py
@@ -0,0 +1,15 @@
+# This program should output "Hello World" once
+class world_class:
+        def __init__(self):
+        self.asleep = True
+    def hello_world(self):
+        if(True):
+            pritn("Hello World")
+        else:
+            print("Goodbye World")
+        while(asleep):
+            print("Goodbye World")
+            asleep = False
+    
+w = world_class
+w.hello_world()
